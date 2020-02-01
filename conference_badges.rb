5217a2a41c26f49378cb9b1d1cdccc29c363c346
Alvaro Armas <aarmas92@outlook.com>
2020-02-01 19:33:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,6 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(speakers)
-  # returns an array of badge messages, using badge_maker
   badge_messages = []
   speakers.each do |speaker|
     message = badge_maker(speaker)
@@ -15,8 +14,6 @@ def batch_badge_creator(speakers)
 end
 
 def assign_rooms(speakers)
-  # assigns each speaker to a room, rooms 1-7.
-  # return a list of room assignments in the form of: "Hello____! You'll be assigned to room ___!"
   room_number = 1
   room_messages = []
   speakers.each do |speaker|
