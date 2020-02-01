d5384267c7cc535fadcffc78d63c0ed66630a5d5
Alvaro Armas <aarmas92@outlook.com>
2020-02-01 19:34:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,7 +24,6 @@ def assign_rooms(speakers)
 end
 
 def printer(speakers)
-  # outputs the results of batch_badge_creator, and assign_rooms
   badge_messages = batch_badge_creator(speakers)
   badge_messages.each do |message|
     puts message
